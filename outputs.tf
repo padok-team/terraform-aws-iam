f4508274eecd450d08e876bad3399d0a364b37a2
Kevin Fairise <kevinf@padok.fr>
2022-01-31 16:29:44 +0100

    fix: output struct

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
 output "iam_roles" {
-  value       = { for role_key, role in aws_iam_role.this : role_key => role.arn }
   description = "Arns of the roles created by the module, format is a map {'role_name': 'role_arn'}"
+  value       = { for role_key, role in aws_iam_role.this : role_key => role.arn }
 }
