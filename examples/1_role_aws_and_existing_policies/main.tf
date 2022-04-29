fe74fa49a0a521dd866c77a7af461b30ae84e987
M22141 <agathe.liguori@ext.bpifrance.fr>
2022-04-29 19:09:58 +0300

    add condition operator, key and value variables

--- examples/1_role_aws_and_existing_policies/main.tf
+++ examples/1_role_aws_and_existing_policies/main.tf
@@ -13,9 +13,9 @@ module "iam" {
   roles = {
     "adminS3" = {
       "assumePrincipal" : "{\"AWS\": \"arn:aws:iam::334033969502:role/lambda_writer\"}"
-      "condition_operator" : var.condition_operator
-      "condition_key" : var.condition_key
-      "condition_value" : var.condition_value
+      "condition_operator" : "${var.condition_operator}"
+      "condition_key" : "${var.condition_key}"
+      "condition_value" : "${var.condition_value}"
       "condition_statement" : "{\"StringEquals\": {\"sts:ExternalId\": datadog_integration_aws.this.external_id }"
       "customPolicies" : ["s3_read_only"],
       "awsManagedPolicies" : ["AmazonS3FullAccess"]
--- variables.tf
+++ variables.tf
@@ -2,7 +2,6 @@ variable "roles" {
   description = "List of the roles to create, for each role you must specify the assume principal, and the policies to bind to it (AWS Managed or custom policies)"
   type = map(object({
     assumePrincipal     = string
-    condition           = string
     assume_condition    = string    
     customPolicies      = list(string)
     awsManagedPolicies  = list(string)
@@ -10,6 +9,23 @@ variable "roles" {
   default = {}
 }
 
+variable "condition_operator" {
+  description = "value"
+  type = string
+  default = ""
+}
+
+variable "condition_key" {
+  description = "value"
+  type = string
+  default = ""
+}
+
+variable "condition_value" {
+  description = "value"
+  type = string
+  default = ""
+}
 variable "policies" {
   description = "Map of the policies to create with format {'policy name' = 'policy'}"
   type        = map(string)
