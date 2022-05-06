162f25f04121ec65cf764674ed125f8c8c7d7a67
Guillaume Leccese <guillaumel@padok.fr>
2022-05-06 19:05:58 +0200

    feat: calling the module to only create 1 role

---
+++ examples/multiple_roles/main.tf
@@ -0,0 +1,147 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+
+  default_tags {
+    tags = {
+      Env         = local.env
+      Region      = local.region
+      OwnedBy     = "Padok"
+      ManagedByTF = true
+    }
+  }
+}
+
+locals {
+
+  name   = "basic"
+  env    = "test"
+  region = "eu-west-3"
+}
+
+data "aws_caller_identity" "current" {}
+
+module "my_role_1" {
+  source = "../../"
+
+  name = "my_role_1"
+
+  principal = jsonencode({
+    "Service" : ["ec2.amazonaws.com", "lambda.amazonaws.com"]
+  })
+
+  # Aws Managed Policies
+  managed_policies = ["AmazonS3FullAccess"]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "ec2:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+output "role_arn_1" {
+  value = module.my_role_1.this.arn
+}
+module "my_role_2" {
+  source = "../../"
+
+  name = "my_role_2"
+
+  principal = jsonencode({
+    "AWS" : "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
+  })
+
+  # Aws Managed Policies
+  managed_policies = ["AmazonS3FullAccess"]
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "rds:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+output "role_arn_2" {
+  value = module.my_role_2.this.arn
+}
+module "my_role_3" {
+  source = "../../"
+
+  name = "my_role_3"
+
+  principal = jsonencode({
+    "AWS" : data.aws_caller_identity.current.account_id
+  })
+
+  # Aws Managed Policies
+  managed_policies = []
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "rds:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+output "role_arn_3" {
+  value = module.my_role_3.this.arn
+}
+
+module "my_role_4" {
+  source = "../../"
+
+  name = "my_role_4"
+
+  principal = jsonencode({
+    "Federated" : "cognito-identity.amazonaws.com"
+  })
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = [
+          "rds:Describe*",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+}
+
+output "role_arn_4" {
+  value = module.my_role_4.this.arn
+}
---
+++ examples/only_managed_policy/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = ">= 1.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = local.region
+
+  default_tags {
+    tags = {
+      Env         = local.env
+      Region      = local.region
+      OwnedBy     = "Padok"
+      ManagedByTF = true
+    }
+  }
+}
+
+locals {
+
+  name   = "basic"
+  env    = "test"
+  region = "eu-west-3"
+}
+
+module "my_role" {
+  source = "../../"
+
+  name = "my_role"
+
+  principal = jsonencode({
+    "Service" : ["ec2.amazonaws.com", "lambda.amazonaws.com"]
+  })
+
+  # Aws Managed Policies
+  managed_policies = ["AmazonS3FullAccess"]
+}
+
+output "role_arn" {
+  value = module.my_role.this.arn
+}
